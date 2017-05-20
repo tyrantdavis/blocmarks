3cd715a933c8de240cadd25d9648b7d14b2fc4e7
Tyrant Davis <davistyrant@gmail.com>
2017-05-20 14:54:02 -0700

    Update welcome controller test. Complete welcome controller testing.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -7,12 +7,22 @@ RSpec.describe WelcomeController, type: :controller do
       get :home
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the home template" do
+      get:home
+      expect(response).to render_template("home")
+    end
   end
 
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      get:index
+      expect(response).to render_template("index")
     end
   end
 
