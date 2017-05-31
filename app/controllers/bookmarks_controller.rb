a75bb79c3d70d12343d5ea3b17d83355f569030b
Tyrant Davis <davistyrant@gmail.com>
2017-05-31 13:11:42 -0700

    Complete bookmarks crud.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,63 @@
 class BookmarksController < ApplicationController
+  before_filter :authenticate_user!
+  def index
+  end
+
   def show
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.new
   end
 
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = @topic.bookmarks.build(bookmark_params)
+
+      if @bookmark.save
+        flash[:notice] = "Bookmark was saved."
+        redirect_to @topic
+      else
+        flash[:error] = "There was an error saving the bookmark. Please try again."
+        render :new
+      end
+   end
+
   def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+   @bookmark = Bookmark.find(params[:id])
+   @bookmark.assign_attributes(bookmark_params)
+
+
+   if @bookmark.save
+     flash[:notice] = "Bookmark was updated."
+     redirect_to topic_path
+   else
+     flash[:error] = "There was an error saving this bookmark. Please try again."
+     render :edit
+   end
+ end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+
+    if @bookmark.destroy
+      flash[:notice] = "\"#{@bookmark.url}\" was deleted successfully."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting the bookmark."
+      render :show
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:url)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,19 @@ class UsersController < ApplicationController
   before_action :require_sign_in
 
   def index
+    @users = User.all
   end
 
   def new
-    @user = User.new
   end
 
   def show
+    if current_user
+      @user = params[:id] ? User.find(params[:id]) : current_user
+      @user_bookmarks = @user.bookmarks
+    else
+      redirect_to new_user_registration_path
+    end
   end
 
 
