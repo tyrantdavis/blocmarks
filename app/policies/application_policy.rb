faec53d0d421a21153cc35f2029a9998c6bdd6fe
Tyrant Davis <davistyrant@gmail.com>
2017-06-01 16:22:55 -0700

    WIP... Update policies and views.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -23,7 +23,7 @@ class ApplicationPolicy
   end
 
   def update?
-    user.present? && (record.user == user || user.admin?)
+    user.present? && ( (record.user == user) || user.admin? )
   end
 
   def edit?
