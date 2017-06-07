3fd64c90e2bccbeca52b8579e37ae2acdedf796e
Tyrant Davis <davistyrant@gmail.com>
2017-06-07 11:51:48 -0700

    Add create# & destroy# to Like controller.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,37 @@
 class LikesController < ApplicationController
   def index
   end
+
+  def create
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    @topic = @bookmark.topic
+    like = current_user.likes.build(bookmark: @bookmark)
+
+    if like.save
+      flash[:notice] = "Bookmark successfully liked."
+      redirect_to [@topic, @bookmark]
+    else
+      flash[:error] = "Bookamrk like failed."
+      redirect_to [@topic, @bookmark]
+    end
+  end
+
+  def destroy
+    # Get the bookmark from the params
+    @bookmark = Bookamrk.find(params[:bookmark_id])
+    @topic = @bookmark.topic
+    # Find the current user's like with the ID in the params
+    like = current_user.likes.build(bookmark: @bookmark)
+
+
+    if like.destroy
+      # Flash success and redirect to @bookmark
+      flash[:notice] = "Bookmark successfully unliked."
+      redirect_to [@topic, @bookmark]
+    else
+      # Flash error and redirect to @bookmark
+      flash[:error] = "Unlike failed. Please try again."
+      redirect_to [@topic, @bookmark]
+    end
+  end
 end
