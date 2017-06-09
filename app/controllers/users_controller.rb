30adb7c6ab0abd01041ecac0f856d323c905ff8a
Tyrant Davis <davistyrant@gmail.com>
2017-06-09 09:56:20 -0700

    Update users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def show
     if current_user
       @user = params[:id] ? User.find(params[:id]) : current_user
-      # @user_bookmarks = @user.bookmarks
+      @user_bookmarks = @user.bookmarks
+      @liked_bookmarks = user.likes
       @topics = @user.topics
     else
       redirect_to new_user_registration_path
