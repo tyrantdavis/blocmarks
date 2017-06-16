7143d68c32a532ee48b24632c514516dabebd35b
Tyrant Davis <davistyrant@gmail.com>
2017-06-15 18:38:43 -0700

    Fix undefined method omniauthable_providers error.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauthable_providers => [:amazon]
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:amazon]
 
          def self.from_omniauth(auth_data)
            where(provider: auth_data.provider, uid: auth_data.uid).first_or_create do |user|
