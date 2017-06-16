9bb1bd0ce3af2df9d77ef579d32cfe5fc87da53d
Tyrant Davis <davistyrant@gmail.com>
2017-06-15 18:31:50 -0700

    Update Devise user route with callbacks.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :users, only: [:show]
-  
+
   resources :topics do
     resources :bookmarks, except: [:index] do
       resources :likes, only: [:index, :create, :destroy]
