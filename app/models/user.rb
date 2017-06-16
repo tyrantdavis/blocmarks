0ba6b9d70a2dcbe63c2e0ed46b2e2545662877eb
Tyrant Davis <davistyrant@gmail.com>
2017-06-15 18:45:40 -0700

    Updates omniauth method's variable User.rb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:amazon]
 
-         def self.from_omniauth(auth_data)
-           where(provider: auth_data.provider, uid: auth_data.uid).first_or_create do |user|
-             user.provider = auth_data.provider
-             user.uid = auth_data.uid
-             user.email = auth_data.info.email
+         def self.from_omniauth(auth)
+           where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+             user.provider = auth.provider
+             user.uid = auth.uid
+             user.email = auth.info.email
              user.password = Devise.friendly_token[6, 20]
            end
          end
