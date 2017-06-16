22e7a26a71773312f3fffe257b48748e9a45d42f
Tyrant Davis <davistyrant@gmail.com>
2017-06-15 18:29:37 -0700

    Add omnauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauthable_providers => [:amazon]
+
+         def self.from_omniauth(auth_data)
+           where(provider: auth_data.provider, uid: auth_data.uid).first_or_create do |user|
+             user.provider = auth_data.provider
+             user.uid = auth_data.uid
+             user.email = auth_data.info.email
+             user.password = Devise.friendly_token[6, 20]
+           end
+         end
 
          has_many :topics, dependent: :destroy
          has_many :bookmarks,  dependent: :destroy
-        has_many :likes, dependent: :destroy
+         has_many :likes, dependent: :destroy
 
          # Virtual attribute for authenticating by either username or email
          # This is in addition to a real persisted field like 'username'
